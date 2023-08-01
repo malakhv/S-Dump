d54dbba1b81adf70459347bd941aa4dcf5c5dec0
Mikhail Malakhov <malakhv@gmail.com>
2023-08-02 00:21:44 +0300

    Small refactoring build.bat

--- build.bat
+++ build.bat
@@ -4,10 +4,17 @@
 :: Author: Mikhail.Malakhov
 ::
 
-rmdir /Q /S build
-mkdir build
+@ECHO OFF
 
 SET PASCAL_KIT="./../PascalKit/src/util"
+
+IF EXIST build\ (
+    echo Clear build directory...
+    rd /s /q build\
+) ELSE (
+    echo The build directory not found...
+)
+mkdir build
 
 fpc.bat ./src/Program.pas -FEbuild ^
     -Fu./src/app ^
