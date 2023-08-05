2ff63d0a6260d8d9aa36ba3ce39c8c678dff3507
Mikhail Malakhov <malakhv@gmail.com>
2023-08-05 12:59:31 +0300

    Start refactoring Dump

--- src/Dump.pas
+++ src/Dump.pas
@@ -52,110 +52,94 @@ function LoadData(const AFile: TFileName; Offset: Integer; Limit: Integer;
 
 implementation
 
-uses ProgMsg;
+uses ProgMsg, Mikhan.Util.StrUtils;
 
-{ Some stuff to print data }
-const 
-    COL_LIMIT = $F;
-    COL_OFFSET = '          ';
-    COL_SEP = '| ';
-    HEADER_SEP = '---------------------------------------------------------';
-
-{ Prints header with bytes offset. }
-procedure PrintHeader();
-var i: Integer;
-begin
-    Write(COL_OFFSET);
-    for i := 0 to COL_LIMIT do
-    begin
-        Write(IntToHex(i, 2));
-        Write(' ');
-    end;
-    WriteLn();
-    WriteLn(HEADER_SEP);
-end;
-
-function MakeChar(Value: Integer): String;
-begin
-    if Value >= 20 then
-        Result := Char(Value) + Char($0)
-    else
-        //Result := IntToHex(Value, 2);
-        Result := '  ';
-end;
-
-procedure PrintChar(Value: Integer);
-begin
-    Write(MakeChar(Value));
-    {if Value >= 20 then
-        Write(Char(Value), Char($0))
-    else
-        Write(IntToHex(Value, 2)); }
-end;
-
-procedure Dump(const Source: Array of Byte; Offset: Integer; Limit: Integer);
+procedure Dump(const Source: Array of Byte; Offset: Integer;
+    Limit: Integer);
 begin
     Dump(Source, OffSet, Limit, ofHex);
 end;
 
-procedure Dump(const Source: Array of Byte; Offset: Integer; Limit: Integer; Format: TOutFormat);
+procedure Dump(const Source: Array of Byte; Offset: Integer;
+    Limit: Integer; Format: TOutFormat);
+const COL_LIMIT = $F;
+
 var 
-    i, col, off, skip: Integer;
-    val: Byte;
-    text: String;
+    col, off, skip: Integer;
+
+    procedure Header();
+    var i: Integer;
+    begin
+        Write(EMPTY:10);
+        for i := 0 to COL_LIMIT do
+            Write(IntToHex(i, 2), CHAR_SPACE);
+        WriteLn();
+        WriteLn(RepeatString('-', 57));
+    end;
 
     procedure NewRow();
     begin
-        if Format = ofHexAndText then Write('    ', text);
-        Writeln('');
-        off := off + COL_LIMIT + 1;
-        Write(IntToHex(off, 8), COL_SEP);
-        col := COL_LIMIT;
-        text := '';
+        WriteLn();
+        Write(IntToHex(off, 8), CHAR_VERT_SLASH, CHAR_SPACE);
     end;
 
-begin
-    PrintHeader();
+    function MakeSymbol(Value: Integer): String;
+    begin
+        // Hex
+        if Format = ofHex then
+        begin
+            Result := IntToHex(Value, 2); Exit;
+        end;
+        // Char
+        if Value >= 20 then
+            Result := Char(Value) + Char($0)
+        else
+            Result := '  ';
+    end;
 
+var
+    val: Byte;
+    i: Integer;
+
+begin
+    Header();
+
+    // Calc initial parameters
     col := COL_LIMIT;
     off := (Offset div 16) + (COL_LIMIT * (Offset div 16));
     skip := (Offset mod 16);
     if Limit <= 0 then Limit := MaxInt;
-    text := '';
-    Write(IntToHex(off, 8), COL_SEP);
+
+    // Start printing
+    NewRow();
     for i := Low(Source) to High(Source) do
     begin
 
-        if col < 0 then NewRow();
+        // Should we go to a new row?
+        if col < 0 then
+        begin
+            off := off + COL_LIMIT + 1;
+            NewRow();
+            col := COL_LIMIT;
+        end;
         Dec(col);
 
+        // Need to skip?
         while skip > 0 do
         begin
-            Write('   ');
-            Dec(skip);
-            Dec(col);
+            Write(EMPTY:3);
+            Dec(skip); Dec(col);
         end;
 
+        // Print value
         val := Source[i];
-        if (Format = ofHex) or (Format = ofHexAndText) then
-        begin
-            Write(IntToHex(val, 2));
-            text := text + MakeChar(val);
-        end else
-            PrintChar(val);
-        Write(' ');
+        Write(MakeSymbol(val), CHAR_SPACE);
         Dec(Limit);
+
+        // Should stop?
         if Limit <= 0 then break;
     end;
-
-    // Process last row
-    if (Format = ofHexAndText) and (text <> '') then
-    begin
-        for i := 0 to col do Write('   ');
-        Write('    ', text);
-    end;
-
-    Writeln('');
+    Writeln();
 end;
 
 function LoadData(const AFile: TFileName; Offset: Integer; Limit: Integer;
