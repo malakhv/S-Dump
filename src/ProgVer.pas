12d71b275db0a9c2de7df311a4c4aa42ef9bb5ac
Mikhail Malakhov <malakhv@gmail.com>
2023-10-04 23:53:28 +0300

    Updated Copyright

--- src/ProgVer.pas
+++ src/ProgVer.pas
@@ -1,31 +1,31 @@
-{--------------------------------------------------------------------}
-{                                                                    }
-{                          S-Dump project                            }
-{                                                                    }
-{  Copyright (C) 1996-2023 Mikhail Malakhov <malakhv@gmail.com>      }
-{                                                                    }
-{  Licensed under the Apache License, Version 2.0 (the "License").   }
-{  You may not use this file except in compliance with the License.  }
-{  You may obtain a copy of the License at                           }
-{                                                                    }
-{     http://www.apache.org/licenses/LICENSE-2.0                     }
-{                                                                    }
-{  Unless required by applicable law or agreed to in writing,        }
-{  software distributed under the License is distributed on an       }
-{  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,      }
-{  either express or implied.                                        }
-{                                                                    }
-{  See the License for the specific language governing permissions   }
-{  and limitations under the License.                                }
-{                                                                    }
-{--------------------------------------------------------------------}
+{-------------------------------------------------------------------------}
+{                                                                         }
+{                             S-Dump project                              }
+{                                                                         }
+{  Copyright (C) 1996-2023 Mikhail Malakhov <malakhv@gmail.com>           }
+{                                                                         }
+{  Licensed under the Apache License, Version 2.0 (the "License").        }
+{  You may not use this file except in compliance with the License.       }
+{  You may obtain a copy of the License at                                }
+{                                                                         }
+{     http://www.apache.org/licenses/LICENSE-2.0                          }
+{                                                                         }
+{  Unless required by applicable law or agreed to in writing, software    }
+{  distributed under the License is distributed on an "AS IS" BASIS,      }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        }
+{  implied.                                                               }
+{                                                                         }
+{  See the License for the specific language governing permissions and    }
+{  limitations under the License.                                         }
+{                                                                         }
+{-------------------------------------------------------------------------}
 
-{--------------------------------------------------------------------}
-{ The Unit contains program version information.                     }
-{                                                                    }
-{ Created: 04.08.2023                                                }
-{ Author: Mikhail.Malakhov                                           }
-{--------------------------------------------------------------------}
+{-------------------------------------------------------------------------}
+{ The Unit contains program version information.                          }
+{                                                                         }
+{ Created: 04.08.2023                                                     }
+{ Author: Mikhail.Malakhov                                                }
+{-------------------------------------------------------------------------}
 
 unit ProgVer;
 
--- src/Program.pas
+++ src/Program.pas
@@ -1,33 +1,33 @@
-{--------------------------------------------------------------------}
-{                                                                    }
-{                          S-Dump project                            }
-{                                                                    }
-{  Copyright (C) 1996-2023 Mikhail Malakhov <malakhv@gmail.com>      }
-{                                                                    }
-{  Licensed under the Apache License, Version 2.0 (the "License").   }
-{  You may not use this file except in compliance with the License.  }
-{  You may obtain a copy of the License at                           }
-{                                                                    }
-{     http://www.apache.org/licenses/LICENSE-2.0                     }
-{                                                                    }
-{  Unless required by applicable law or agreed to in writing,        }
-{  software distributed under the License is distributed on an       }
-{  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,      }
-{  either express or implied.                                        }
-{                                                                    }
-{  See the License for the specific language governing permissions   }
-{  and limitations under the License.                                }
-{                                                                    }
-{--------------------------------------------------------------------}
+{-------------------------------------------------------------------------}
+{                                                                         }
+{                             S-Dump project                              }
+{                                                                         }
+{  Copyright (C) 1996-2023 Mikhail Malakhov <malakhv@gmail.com>           }
+{                                                                         }
+{  Licensed under the Apache License, Version 2.0 (the "License").        }
+{  You may not use this file except in compliance with the License.       }
+{  You may obtain a copy of the License at                                }
+{                                                                         }
+{     http://www.apache.org/licenses/LICENSE-2.0                          }
+{                                                                         }
+{  Unless required by applicable law or agreed to in writing, software    }
+{  distributed under the License is distributed on an "AS IS" BASIS,      }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        }
+{  implied.                                                               }
+{                                                                         }
+{  See the License for the specific language governing permissions and    }
+{  limitations under the License.                                         }
+{                                                                         }
+{-------------------------------------------------------------------------}
 
-{--------------------------------------------------------------------}
-{ Small program to see and save to external file a dump (like in hex }
-{ editors) of any file. It can be useful to investigate file formats }
-{ and reverse engineering.                                           }
-{                                                                    }
-{ Created: 03.11.2022                                                }
-{ Author: Mikhail.Malakhov                                           }
-{--------------------------------------------------------------------}
+{-------------------------------------------------------------------------}
+{ Small program to see and save to external file a dump (like in hex      }
+{ editors) of any file. It can be useful to investigate file formats and  }
+{ reverse engineering.                                                    }
+{                                                                         }
+{ Created: 03.11.2022                                                     }
+{ Author: Mikhail.Malakhov                                                }
+{-------------------------------------------------------------------------}
 
 program sdump;
 
